df29af1706378fd09938faf4633988038d0730c7
Nomad/이용희 <goodyhlee@coupang.com>
2017-12-28 17:37:18 +0900

    multifile upload - 하나의 파일 업로드 테스트 성공

--- src/main/java/com/photoal/webapp/web/FileUploadController.java
+++ src/main/java/com/photoal/webapp/web/FileUploadController.java
@@ -1,28 +1,26 @@
 package com.photoal.webapp.web;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
-
+import com.photoal.webapp.service.storage.StorageFileNotFoundException;
+import com.photoal.webapp.service.storage.StorageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.photoal.webapp.service.storage.StorageFileNotFoundException;
-import com.photoal.webapp.service.storage.StorageService;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
+@Slf4j
 public class FileUploadController {
 
     private final StorageService storageService;
@@ -53,19 +51,38 @@ public class FileUploadController {
     }
 
     @PostMapping("/photos")
-    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+    public String handleFileUpload(@RequestParam("file") MultipartFile[] files,
             RedirectAttributes redirectAttributes) {
 
-        storageService.store(file);
+        List<String> fileNames = new ArrayList<>();
+        for (MultipartFile file : files) {
+            storageService.store(file);
+            fileNames.add(file.getOriginalFilename());
+}
+
+        if (fileNames.size() <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        log.info("#### Uploaded File => {}", fileNames.toString());
+
+
         redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
+                "You successfully uploaded [" + fileNames.toString() + "]!");
 
         return "redirect:/photos";
     }
 
-    @ExceptionHandler(StorageFileNotFoundException.class)
-    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
-        return ResponseEntity.notFound().build();
+    @ExceptionHandler({StorageFileNotFoundException.class,IllegalArgumentException.class})
+    public ResponseEntity<?> handleStorageFileNotFound(Throwable exc) {
+        if (exc instanceof StorageFileNotFoundException) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+
     }
+
+
 
 }
